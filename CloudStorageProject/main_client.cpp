1c71edd56f51827b42687076d9a839d6260941f5
giacomovitangeli <giacomo.vitangeli@gmail.com>
2022-05-13 17:52:26 +0200

    socket and first client server message

--- CloudStorageProject/main_client.cpp
+++ CloudStorageProject/main_client.cpp
@@ -1,20 +1,96 @@
-#include <iostream>
-#include <cstdlib>
-#include <openssl/rand.h>
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <malloc.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <ifaddrs.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
-using namespace std;
-int main() {
- 	RAND_poll();
- 	unsigned char key[16];
- 	unsigned char iv[16];
-	 RAND_bytes(key, 16);
-	 RAND_bytes(iv, 16);
- 	/* proceeds with encryption */
-	for(int i=0; i<16; i++){
-		cout<<key[i]<<endl;
+int main(){
+	int socket1 = 0, len = 0, ret = 0, i = 0;
+	char *rcv_msg = 0, *resp_msg = 0;
+    uint16_t lmsg;
+    struct sockaddr_in sv_addr;
+
+	/*	Pulizia e inizializzazione strutture client	 */
+	memset(&sv_addr, 0, sizeof(sv_addr)); 
+	sv_addr.sin_family = AF_INET;
+    sv_addr.sin_port = 4242;//htons(atoi(4242));
+    if((ret = inet_pton(AF_INET, "127.0.0.1", &(sv_addr.sin_addr))) == 0){
+		printf("Formato indirizzo non valido!\n");
+		exit(0);
 	}
-return 0;
+    socket1 = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket1 < 0){
+        printf("Errore nellacreazione del socket di connessione!\n");
+        exit(0);
+    }
+    printf(">>> Socket creato correttamente!\n"); 
+    ret = connect(socket1, (struct sockaddr*)&sv_addr, sizeof(sv_addr));
+    if(ret < 0){
+        printf("Errore di connessione!\n");
+        exit(0);
+    }
+	
+	while(1){
+		resp_msg = (char*)malloc(128);
+		if(!resp_msg){
+			printf("Malloc failed!\n");
+			exit(0);
+		}
+        rcv_msg = (char*)malloc(128);
+		if(!rcv_msg){
+			printf("Malloc failed!\n");
+			exit(0);
+		}
+		if(i == 0){
+			printf("In attesa di un comando...\n");
+			fgets(resp_msg, 128, stdin);
+			fflush(stdin);
+		
+			len = strlen(resp_msg);
+			lmsg = htons(len);
+			ret = send(socket1, (void*)&lmsg, sizeof(uint16_t), 0);
+			if(ret < 0){
+				printf("Errore nella send di lmsg!\n");
+				exit(0);
+			}
+			ret = send(socket1, (void*)resp_msg, len, 0);
+			if(ret < 0){
+				printf("Errore nella send del buffer!\n");
+				exit(0);
+			}
+			i = 1;
+		}
+		ret = recv(socket1, (void*)&lmsg, sizeof(uint16_t), 0);
+		if(ret == 0){
+			printf(">>> Connessione col server persa!\n");
+			close(socket1);
+			exit(0);
+		}
+		if(ret < 0){
+			printf("Errore nella ricezione della risposta (dim) dal server (1)!\n");
+			exit(0);
+		}
+		len = ntohs(lmsg);
+		rcv_msg = (char*)malloc(len);
+		if(!rcv_msg){
+			printf("Malloc failed!\n");
+			exit(0);
+		}
+		ret = recv(socket1, (void*)rcv_msg, len, 0);
+		if(ret < 0){
+			printf("Errore nella ricezione della risposta (buf) dal server(1)!\n");
+			exit(0);
+		}
+		i = 0;
+	}
+	return 0; // inutile
 }
--- CloudStorageProject/main_server.cpp
+++ CloudStorageProject/main_server.cpp
@@ -1,7 +1,120 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <malloc.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <ifaddrs.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
-using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main(){
+	int listner_socket = 0, new_socket = 0, ret = 0, option = 1, k = 0, fdmax;
+	char *resp_msg = 0, *rcv_msg = 0;
+	uint16_t lmsg; //fixme *len;
+	struct sockaddr_in my_addr, client_addr;
+	fd_set master; 
+	fd_set read_set;
+
+
+	FD_ZERO(&master);
+	FD_ZERO(&read_set);
+
+	listner_socket = socket(AF_INET, SOCK_STREAM, 0); // Creazione listner
+    setsockopt(listner_socket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));	//permette di riusare il solito indirizzo per il socket, necessario per provare senza dover spengere la virtual machine
+    if(listner_socket < 0){
+        printf("Errore nella creazione del socket di comunicazione!\n");
+        exit(0);
+    }
+    printf(">>> Socket creato correttamente!\n");
+	
+    /*	Pulizia e inizializzazione strutture server	 */
+    memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(4242); //4242 porta CASUALE
+    my_addr.sin_addr.s_addr = INADDR_ANY; // vincola il socket a tutte le interfacce, usare inet_addr("127.0.0.1"); se si vuole utilizzare il solo indirizzo locale
+
+    ret = bind(listner_socket, (struct sockaddr*)&my_addr, sizeof(my_addr));
+    if(ret < 0){
+        perror("Errore nella bind: \n");
+        exit(0);
+    }
+    
+    ret = listen(listner_socket, 10); // 10 = numero massimo di connessioni in attesa, l'11esima verrà rifiutata -- 10 numero CASUALE
+    if(ret < 0){
+        perror("Errore nella listen: \n");
+        exit(0);
+    }
+    printf(">>> Socket associato correttamente all'indirizzo!\n");
+	FD_SET(listner_socket, &master);	// Aggiungo il listner al master
+	fdmax = listner_socket;
+    len = sizeof(client_addr);
+
+
+	while(1){	//Processo unico, client multipli gestiti tramite select()
+		read_set = master;
+		select(fdmax + 1, &read_set, NULL, NULL, NULL);
+		for(k = 0; k <= fdmax; k++){
+			if(FD_ISSET(k, &read_set)){
+				if(k == listner_socket){ // Sono il listner
+					printf(">>> In attesa di connessione...\n\n");
+        			new_socket = accept(listner_socket, (struct sockaddr*)&client_addr, len);
+       				if(new_socket < 0){
+            			perror("Errore nella creazione del socket di comunicazione!\n");
+            			exit(0);
+        			}
+					FD_SET(new_socket, &master);
+					if(new_socket > fdmax)
+						fdmax = new_socket;
+				}
+				else{	// Non sono il listner
+					printf(">>> In attesa di un comando dal client...\n\n");
+					/* Alloco spazio per i messaggi sia in entrata che in uscita */
+					rcv_msg = (char*)malloc(128);
+					if(!rcv_msg)
+						exit(0);
+					resp_msg = (char*)malloc(128);
+					if(!resp_msg)
+						exit(0);
+					/* RICEVO LA LUNGHEZZA DEL MESSAGGIO */
+					ret = recv(k, (void*)&lmsg, sizeof(uint16_t), 0);
+					if(ret == 0){
+						printf("Comunicazione col client interrotta!\n");
+						exit(0);
+					}
+					if(ret < 0){
+		                printf("Errore nella recv!\n");
+		                exit(0);
+		            }
+					/* RICEVO IL MESSAGGIO */
+					len = ntohs(lmsg);
+					ret = recv(k, (void*)rcv_msg, len, 0);
+					if(ret == 0){
+						printf("Comunicazione col client interrotta!\n");
+						exit(0);
+					}
+		            if(ret < 0){
+		                printf("Errore nella recv!\n");
+		                exit(0);
+		            }
+					printf("%s", rcv_msg);
+		            fgets(resp_msg, 128, stdin);
+					len = strlen(resp_msg);
+		            lmsg = htons(len);
+		            send(k, (void*)&lmsg, sizeof(uint16_t), 0);
+		            send(k, (void*)resp_msg, len, 0);
+					
+					memset(rcv_msg, 0, 128);
+					memset(resp_msg, 0, 128);
+				}
+			}
+		}
+	}
+					
+	return 0; // inutile
 }
